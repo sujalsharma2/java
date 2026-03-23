e9031b2c7da70c0903d11e0856b841080bf796a1
sujal sharma <131173277+sujalsharma2@users.noreply.github.com>
2026-03-23 10:18:27 +0530

    input output

---
+++ InOut.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class InOut {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //nextInt() - for integer input
+        //nextDouble() - for double input
+        //next() - for string input
+        //nextLine() - for string input with spaces
+        //nextFloat() - for float input
+
+        String name  = sc.next();
+        System.out.println(name);
+    }
+}
