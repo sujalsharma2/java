2e10bd409e3a1d2c9d4ba1d4e06062813a89abe3
sujal sharma <131173277+sujalsharma2@users.noreply.github.com>
2026-03-23 11:51:08 +0530

    day 1

--- loops.java
+++ loops.java
@@ -1,3 +1,43 @@
+import java.util.*;
 public class loops {
-    
+     public static void main(String[] args) {
+        
+        /*for (int i = 0; i < 11; i++) {
+            System.out.println(i);
+        }*/
+
+            /*(int i = 0;
+            while (i < 11) {
+                System.out.println(i);
+                i++;
+            } */
+
+        /*int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 11); */
+
+
+        // Q1: print the sum of first n natural numbers
+
+        /* Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+        */
+       // Q2: print the Table of a number input by user
+       Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+       for (int i = 1; i < 11; i++) {
+        System.out.println( n * i);
+       }
+
+
+
+     }
 }
